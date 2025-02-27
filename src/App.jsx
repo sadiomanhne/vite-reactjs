fb8666d1049caffb53447152696f95d8b3689190
trandangtuan <trandangtuan28@gmail.com>
2025-02-27 21:12:59 +0700

    Adding Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import AntdTable from "./component/Table";
 
 function App() {
-  const [tableData, setTableData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://run.mocky.io/v3/d9870639-fb48-42dc-ada7-fd6ef256e938"
-        );
-        const data = await response.json();
-        setTableData(data);
-      } catch (error) {
-        console.error("Có lỗi:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <>
-      <AntdTable data={tableData} />
+      <AntdTable />
     </>
   );
 }
--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,30 +1,55 @@
 import { Table } from "antd";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
-const AntdTable = ({ data }) => {
+
+const AntdTable = () => {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://67bfced4b9d02a9f22474c36.mockapi.io/api/v1/users"
+        );
+        const data = await response.json();
+        setTableData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   const columns = [
     {
-      title: "Company",
-      dataIndex: "Company",
-      key: "company",
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
     },
     {
-      title: "Contact",
-      dataIndex: "Contact",
-      key: "contact",
+      title: "Date of Birth",
+      dataIndex: "dateOfBirth",
+      key: "dateOfBirth",
+      render: (text) => moment(text).format("YYYY/MM/DD"),
     },
     {
-      title: "Country",
-      dataIndex: "Country",
-      key: "country",
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
     },
-    
   ];
+  const paginationConfig = {
+    pageSizeOptions: ["10", "20", "50"],
+    showSizeChanger: true,
+    defaultPageSize: 10,
+  };
 
   return (
     <Table
-      dataSource={data}
+      dataSource={tableData}
       columns={columns}
-      rowKey={(record) => record.id || record.Company}
+      pagination={paginationConfig}
     />
   );
 };
